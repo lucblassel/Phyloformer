f7bfcbfdc31dfc7bbdf87159168caec1fe8c9f5b
Luc Blassel <luc.blassel@gmail.com>
2023-03-20 20:01:18 +0100

    Adding sphinx documentation

---
+++ docs/conf.py
@@ -0,0 +1,57 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+version = None
+with open("../phyloformer/__init__.py", "r") as file:
+    for line in file:
+        if line.startswith("__version__"):
+            version = line.split('"')[1].strip()
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Phyloformer'
+copyright = '2023, Luca Nesterenko'
+author = 'Luca Nesterenko, Bastien Bousseau, Laurent Jacob'
+release = version
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'm2r2',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+source_suffix = ['.rst', '.md']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+# # Autodoc settings
+#autodoc_default_options = {
+#    "members": True,
+#    "undoc-members": True,
+#    "private-members": True
+#}
+autoclass_content = 'both'
---
+++ docs/phyloformer.rst
@@ -0,0 +1,62 @@
+phyloformer package
+===================
+
+..
+    Subpackages
+    -----------
+
+    .. toctree::
+    :maxdepth: 4
+
+    phyloformer.scripts
+
+Submodules
+----------
+
+phyloformer.attentions module
+-----------------------------
+
+.. automodule:: phyloformer.attentions
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+phyloformer.data module
+-----------------------
+
+.. automodule:: phyloformer.data
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+phyloformer.phyloformer module
+------------------------------
+
+.. automodule:: phyloformer.phyloformer
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+phyloformer.pretrained module
+-----------------------------
+
+.. automodule:: phyloformer.pretrained
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+phyloformer.training module
+---------------------------
+
+.. automodule:: phyloformer.training
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: phyloformer
+   :members:
+   :undoc-members:
+   :show-inheritance:
